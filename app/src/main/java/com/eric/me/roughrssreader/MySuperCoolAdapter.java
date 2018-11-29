295550c45def2e2d83460347607016128c1599f5
Eric Liu <eric1932777@gmail.com>
2018-11-29 00:59:01 -0600

    除了图片和点击都没问题了

--- app/src/main/java/com/eric/me/roughrssreader/MySuperCoolAdapter.java
+++ app/src/main/java/com/eric/me/roughrssreader/MySuperCoolAdapter.java
@@ -1,4 +1,93 @@
 package com.eric.me.roughrssreader;
 
-public class MySuperCoolAdapter {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.prof.rssparser.Article;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MySuperCoolAdapter extends RecyclerView.Adapter<MySuperCoolAdapter.ViewHolder> {
+
+    private ArrayList<Article> articleS = new ArrayList<>();
+    private int rowNums;
+    //不知道下面这个什么意思
+    //TODO why never used
+    private Context myDontKnowContext;
+
+    public MySuperCoolAdapter(ArrayList<Article> articleList, int n, Context context) {
+        articleS = articleList;
+        rowNums = n;
+        myDontKnowContext = context;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+        TextView date;
+        TextView intro;
+        ImageView headPicture;
+
+        //TODO private
+        public ViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.textViewTitle);
+            date = itemView.findViewById(R.id.textViewDate);
+            intro = itemView.findViewById(R.id.textViewIntro);
+            headPicture = itemView.findViewById(R.id.imageHeadPicture);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(rowNums, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    //最重要的部分
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int index) {
+        Article currentArticle = articleS.get(index);
+        String titleStr = currentArticle.getTitle();
+        String dateStr = currentArticle.getPubDate().toString();
+        String introStr = currentArticle.getDescription();
+        String imageURL = currentArticle.getImage();
+
+        viewHolder.title.setText(titleStr);
+        viewHolder.date.setText(dateStr);
+        viewHolder.intro.setText(introStr);
+        // to show image
+
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Chrome Webkit
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (articleS == null) {
+            return -1;
+        } else {
+            return articleS.size();
+        }
+    }
 }
--- app/src/main/java/com/eric/me/roughrssreader/RSSTestActivity.java
+++ app/src/main/java/com/eric/me/roughrssreader/RSSTestActivity.java
@@ -2,8 +2,12 @@ package com.eric.me.roughrssreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.prof.rssparser.Article;
 import com.prof.rssparser.Parser;
@@ -14,43 +18,44 @@ import java.util.ArrayList;
 
 public class RSSTestActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private MySuperCoolAdapter mySuperCoolAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rsstest);
+
+        //Load RecyclerView
+        recyclerView = findViewById(R.id.theREcyclerVIEW);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 
     public void onClickMain(View v) {
         //TODO 替换为自定义网址
         String urlString = "https://www.androidcentral.com/feed";
-        retrieve(urlString);
+        retrieveFeed(urlString);
     }
 
     public void onClickCurl(View v) {
 
     }
 
-    public void retrieve(String urlString) {
-        final TextView tv = findViewById(R.id.textViewRSS);
-        tv.setText("");
+    public void retrieveFeed(String urlString) {
         Parser parser = new Parser();
         parser.execute(urlString);
         parser.onFinish(new Parser.OnTaskCompleted() {
-
             @Override
-            public void onTaskCompleted(ArrayList<Article> list) {
-                //what to do when the parsing is done
-                //the Array List contains all article's data. For example you can use it for your adapter.
-                for (Article x : list) {
-                    tv.setText(tv.getText() + "" + x + "\n");
-                }
+            public void onTaskCompleted(ArrayList<Article> arrayList) {
+                mySuperCoolAdapter = new MySuperCoolAdapter(arrayList, R.layout.card, RSSTestActivity.this);
+                recyclerView.setAdapter(mySuperCoolAdapter);
             }
 
             @Override
             public void onError() {
-                //what to do in case of error
+                Toast.makeText(getApplicationContext(), "SOME ERROR OCCURRED", Toast.LENGTH_LONG).show();
             }
         });
-        return;
     }
 }
