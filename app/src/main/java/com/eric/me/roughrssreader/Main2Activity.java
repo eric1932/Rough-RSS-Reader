91a3ffe23186ebdc859594db5a51af76f5122c29
Zaitian Wang <zaitian3@illinois.edu>
2018-12-05 17:14:59 -0600

    lll

---
+++ app/src/main/java/com/eric/me/roughrssreader/Main2Activity.java
@@ -0,0 +1,30 @@
+package com.eric.me.roughrssreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    protected void onStart() {
+        super.onStart();
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onStop() {
+        super.onStop();
+    }
+
+}
